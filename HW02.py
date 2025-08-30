251e545c6d00524ce9ce8a0c6d6b33d6a28344d3
r3dr000ck <keigo.momo.momo@gmail.com>
2025-08-29 21:12:21 -0400

    HW02

---
+++ HW02.py
@@ -0,0 +1,66 @@
+"""
+Georgia Institute of Technology - CS1301
+Homework 2 - Conditionals
+"""
+
+#########################################
+
+"""
+Function Name: buyTickets()
+Parameters: num_people (int), ticket_section (str), days_before (int)
+Returns: total_price (str)
+"""
+
+def buyTickets():
+    pass
+
+
+#########################################
+
+"""
+Function Name: matchStatus()
+Parameters: player_games_won (int), opponent_games_won (int)
+Returns: status (str)
+"""
+
+def matchStatus():
+    pass
+
+
+#########################################
+
+"""
+Function Name: tiebreak()
+Parameters: player_1 (str), player_2 (str), score_1 (int), score_2 (int)
+Returns: winner (str)
+"""
+
+def tiebreak():
+    pass
+
+
+#########################################
+
+"""
+Function Name: playerStats()
+Parameters: player_name (str), aces (int), double_faults (int), winners (int), unforced_errors (int)
+Returns: player_performance (str)
+"""
+
+def playerStats():
+    pass
+
+
+#########################################
+
+"""
+Function Name: tennisPredictor()
+Parameters: player_1 (str), player_2 (str), stats_1 (int), stats_2 (int), head_to_head (str), matches_played_1 (int), matches_played_2 (int)
+Returns: prediction (str)
+"""
+
+def tennisPredictor():
+    pass
+
+
+#########################################
