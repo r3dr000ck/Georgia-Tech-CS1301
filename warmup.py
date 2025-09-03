5d6787d51497a325e76ec6795fa713a4937fae68
r3dr000ck <keigo.momo.momo@gmail.com>
2025-09-03 16:29:18 -0400

    warmup

---
+++ warmup.py
@@ -0,0 +1,6 @@
+def techLibs():
+    name = input("Enter your name: ")
+    bf = input("Enter your best friend's name: ")
+    sp = input("Enter your favorite study spot: ")
+    print(f"The other day, I saw {name} and {bf} up to no good.")
+    print(f"They were loudly gossiping at {sp} instead of studying.")
